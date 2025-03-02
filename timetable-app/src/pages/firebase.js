b090b2d5386e811a02fd3035b74ee76065da2114
Harsh-satkar <114323600+Harsh-satkar@users.noreply.github.com>
2025-03-02 12:26:47 +0530

    Update firebase.js

--- timetable-app/src/pages/firebase.js
+++ timetable-app/src/pages/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore, collection, addDoc } from "firebase/firestore";
 import dotenv from 'dotenv';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: "AIzaSyDGLfo5fSsnDNwg-m6lgu95zFSBv2xFxeg",
+  authDomain: "timetable-app-7ff8d.firebaseapp.com",
+  projectId: "timetable-app-7ff8d",
+  storageBucket: "timetable-app-7ff8d.firebasestorage.app",
+  messagingSenderId: "404137538059",
+  appId: "1:404137538059:web:891cd9dac74c7a8ecb8d0e"
 };
 
 // Initialize Firebase
